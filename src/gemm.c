48816f114d4b1efe342635fe5fc2e127a086a282
Amogh Akshintala <aakshintala@cs.stonybrook.edu>
2019-02-06 09:41:07 -0600

    fix some stragglers

--- src/gemm.c
+++ src/gemm.c
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include <math.h>
 
-void gemm_bin(int M, int N, int K, float ALPHA, 
-        char  *A, int lda, 
+void gemm_bin(int M, int N, int K, float ALPHA,
+        char  *A, int lda,
         float *B, int ldb,
         float *C, int ldc)
 {
@@ -62,8 +62,8 @@ void time_random_matrix(int TA, int TB, int m, int k, int n)
 }
 
 
-void gemm(int TA, int TB, int M, int N, int K, float ALPHA, 
-        float *A, int lda, 
+void gemm(int TA, int TB, int M, int N, int K, float ALPHA,
+        float *A, int lda,
         float *B, int ldb,
         float BETA,
         float *C, int ldc)
@@ -71,8 +71,8 @@ void gemm(int TA, int TB, int M, int N, int K, float ALPHA,
     gemm_cpu( TA,  TB,  M, N, K, ALPHA,A,lda, B, ldb,BETA,C,ldc);
 }
 
-void gemm_nn(int M, int N, int K, float ALPHA, 
-        float *A, int lda, 
+void gemm_nn(int M, int N, int K, float ALPHA,
+        float *A, int lda,
         float *B, int ldb,
         float *C, int ldc)
 {
@@ -88,8 +88,8 @@ void gemm_nn(int M, int N, int K, float ALPHA,
     }
 }
 
-void gemm_nt(int M, int N, int K, float ALPHA, 
-        float *A, int lda, 
+void gemm_nt(int M, int N, int K, float ALPHA,
+        float *A, int lda,
         float *B, int ldb,
         float *C, int ldc)
 {
@@ -106,8 +106,8 @@ void gemm_nt(int M, int N, int K, float ALPHA,
     }
 }
 
-void gemm_tn(int M, int N, int K, float ALPHA, 
-        float *A, int lda, 
+void gemm_tn(int M, int N, int K, float ALPHA,
+        float *A, int lda,
         float *B, int ldb,
         float *C, int ldc)
 {
@@ -123,8 +123,8 @@ void gemm_tn(int M, int N, int K, float ALPHA,
     }
 }
 
-void gemm_tt(int M, int N, int K, float ALPHA, 
-        float *A, int lda, 
+void gemm_tt(int M, int N, int K, float ALPHA,
+        float *A, int lda,
         float *B, int ldb,
         float *C, int ldc)
 {
@@ -142,8 +142,8 @@ void gemm_tt(int M, int N, int K, float ALPHA,
 }
 
 
-void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA, 
-        float *A, int lda, 
+void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA,
+        float *A, int lda,
         float *B, int ldb,
         float BETA,
         float *C, int ldc)
@@ -169,18 +169,24 @@ void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA,
 
 #include <math.h>
 
-void gemm_gpu(int TA, int TB, int M, int N, int K, float ALPHA, 
-        float *A_gpu, int lda, 
+void gemm_gpu(int TA, int TB, int M, int N, int K, float ALPHA,
+        float *A_gpu, int lda,
         float *B_gpu, int ldb,
         float BETA,
-        float *C_gpu, int ldc)
+        float *C_gpu, int ldc, cudaStream_t *stream)
 {
     cublasHandle_t handle = blas_handle();
-    cudaError_t status = cublasSgemm(handle, (TB ? CUBLAS_OP_T : CUBLAS_OP_N), 
+    cudaStream_t currentStream;
+    if (cublasGetStream(handle, &currentStream) == CUBLAS_STATUS_SUCCESS) {
+        if (currentStream != *stream)
+            cublasSetStream(handle, *stream);
+    }
+    cudaError_t status = cublasSgemm(handle, (TB ? CUBLAS_OP_T : CUBLAS_OP_N),
             (TA ? CUBLAS_OP_T : CUBLAS_OP_N), N, M, K, &ALPHA, B_gpu, ldb, A_gpu, lda, &BETA, C_gpu, ldc);
     check_error(status);
 }
 
+#if 0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -210,39 +216,39 @@ void time_gpu_random_matrix(int TA, int TB, int m, int k, int n)
     free(c);
 }
 
-void time_gpu(int TA, int TB, int m, int k, int n)
-{
-    int iter = 10;
-    float *a = random_matrix(m,k);
-    float *b = random_matrix(k,n);
+// void time_gpu(int TA, int TB, int m, int k, int n)
+// {
+//     int iter = 10;
+//     float *a = random_matrix(m,k);
+//     float *b = random_matrix(k,n);
 
-    int lda = (!TA)?k:m;
-    int ldb = (!TB)?n:k;
+//     int lda = (!TA)?k:m;
+//     int ldb = (!TB)?n:k;
 
-    float *c = random_matrix(m,n);
+//     float *c = random_matrix(m,n);
 
-    float *a_cl = cuda_make_array(a, m*k);
-    float *b_cl = cuda_make_array(b, k*n);
-    float *c_cl = cuda_make_array(c, m*n);
+//     float *a_cl = cuda_make_array(a, m*k);
+//     float *b_cl = cuda_make_array(b, k*n);
+//     float *c_cl = cuda_make_array(c, m*n);
 
-    int i;
-    clock_t start = clock(), end;
-    for(i = 0; i<iter; ++i){
-        gemm_gpu(TA,TB,m,n,k,1,a_cl,lda,b_cl,ldb,1,c_cl,n);
-        cudaThreadSynchronize();
-    }
-    double flop = ((double)m)*n*(2.*k + 2.)*iter;
-    double gflop = flop/pow(10., 9);
-    end = clock();
-    double seconds = sec(end-start);
-    printf("Matrix Multiplication %dx%d * %dx%d, TA=%d, TB=%d: %lf s, %lf GFLOPS\n",m,k,k,n, TA, TB, seconds, gflop/seconds);
-    cuda_free(a_cl);
-    cuda_free(b_cl);
-    cuda_free(c_cl);
-    free(a);
-    free(b);
-    free(c);
-}
+//     int i;
+//     clock_t start = clock(), end;
+//     for(i = 0; i<iter; ++i){
+//         gemm_gpu(TA,TB,m,n,k,1,a_cl,lda,b_cl,ldb,1,c_cl,n);
+//         cudaThreadSynchronize();
+//     }
+//     double flop = ((double)m)*n*(2.*k + 2.)*iter;
+//     double gflop = flop/pow(10., 9);
+//     end = clock();
+//     double seconds = sec(end-start);
+//     printf("Matrix Multiplication %dx%d * %dx%d, TA=%d, TB=%d: %lf s, %lf GFLOPS\n",m,k,k,n, TA, TB, seconds, gflop/seconds);
+//     cuda_free(a_cl);
+//     cuda_free(b_cl);
+//     cuda_free(c_cl);
+//     free(a);
+//     free(b);
+//     free(c);
+// }
 
 
 void test_gpu_accuracy(int TA, int TB, int m, int k, int n)
@@ -282,43 +288,44 @@ void test_gpu_accuracy(int TA, int TB, int m, int k, int n)
     free(c_gpu);
 }
 
-int test_gpu_blas()
-{
+// int test_gpu_blas()
+// {
     /*
-       test_gpu_accuracy(0,0,10,576,75); 
+       test_gpu_accuracy(0,0,10,576,75);
 
-       test_gpu_accuracy(0,0,17,10,10); 
-       test_gpu_accuracy(1,0,17,10,10); 
-       test_gpu_accuracy(0,1,17,10,10); 
-       test_gpu_accuracy(1,1,17,10,10); 
+       test_gpu_accuracy(0,0,17,10,10);
+       test_gpu_accuracy(1,0,17,10,10);
+       test_gpu_accuracy(0,1,17,10,10);
+       test_gpu_accuracy(1,1,17,10,10);
 
-       test_gpu_accuracy(0,0,1000,10,100); 
-       test_gpu_accuracy(1,0,1000,10,100); 
-       test_gpu_accuracy(0,1,1000,10,100); 
-       test_gpu_accuracy(1,1,1000,10,100); 
+       test_gpu_accuracy(0,0,1000,10,100);
+       test_gpu_accuracy(1,0,1000,10,100);
+       test_gpu_accuracy(0,1,1000,10,100);
+       test_gpu_accuracy(1,1,1000,10,100);
 
-       test_gpu_accuracy(0,0,10,10,10); 
+       test_gpu_accuracy(0,0,10,10,10);
 
-       time_gpu(0,0,64,2916,363); 
-       time_gpu(0,0,64,2916,363); 
-       time_gpu(0,0,64,2916,363); 
-       time_gpu(0,0,192,729,1600); 
-       time_gpu(0,0,384,196,1728); 
-       time_gpu(0,0,256,196,3456); 
-       time_gpu(0,0,256,196,2304); 
-       time_gpu(0,0,128,4096,12544); 
-       time_gpu(0,0,128,4096,4096); 
+       time_gpu(0,0,64,2916,363);
+       time_gpu(0,0,64,2916,363);
+       time_gpu(0,0,64,2916,363);
+       time_gpu(0,0,192,729,1600);
+       time_gpu(0,0,384,196,1728);
+       time_gpu(0,0,256,196,3456);
+       time_gpu(0,0,256,196,2304);
+       time_gpu(0,0,128,4096,12544);
+       time_gpu(0,0,128,4096,4096);
      */
-    time_gpu(0,0,64,75,12544); 
-    time_gpu(0,0,64,75,12544); 
-    time_gpu(0,0,64,75,12544); 
-    time_gpu(0,0,64,576,12544); 
-    time_gpu(0,0,256,2304,784); 
-    time_gpu(1,1,2304,256,784); 
-    time_gpu(0,0,512,4608,196); 
-    time_gpu(1,1,4608,512,196); 
+//     time_gpu(0,0,64,75,12544);
+//     time_gpu(0,0,64,75,12544);
+//     time_gpu(0,0,64,75,12544);
+//     time_gpu(0,0,64,576,12544);
+//     time_gpu(0,0,256,2304,784);
+//     time_gpu(1,1,2304,256,784);
+//     time_gpu(0,0,512,4608,196);
+//     time_gpu(1,1,4608,512,196);
 
-    return 0;
-}
+//     return 0;
+// }
+#endif
 #endif
 
